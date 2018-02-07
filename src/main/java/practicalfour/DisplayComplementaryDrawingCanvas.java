9d3971457f0134596a7c07882f11ddad54e90e95
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-02-07 15:39:27 -0500

    Practical 4 files

---
+++ src/main/java/practicalfour/DisplayComplementaryDrawingCanvas.java
@@ -0,0 +1,65 @@
+package practicalfour;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Date;
+import java.util.Scanner;
+import javax.swing.JFrame;
+
+/**
+ * This class uses another class to produce simple graphical output.
+ * The program will display a complementary color scheme.
+ *
+ * <p>Bugs: This program has no known bugs, but is missing code.
+ *
+ * @author Janyl Jumadinova
+ */
+public class DisplayComplementaryDrawingCanvas {
+
+  // declare variables that can store the user's color values
+  public static int redValue;
+  public static int greenValue;
+  public static int blueValue;
+
+  // define the CANVAS_HEIGHT and CANVAS_WIDTH of the graphic
+  public static final int CANVAS_WIDTH = 600;
+  public static final int CANVAS_HEIGHT = 400;
+
+  /**
+   * The main method will input values from a file and display the canvas.
+   * The program will display a complementary color scheme, using some
+   * methods that are provided by another Java class.
+   *
+   * <p>Bugs: This program has no known bugs.
+   *
+   * @author Janyl Jumadinova
+   */
+  public static void main(String[] args) {
+    // declare the starting file and scanner
+    File colorValuesInputFile = null;
+    Scanner scanner = null;
+    // connect the scanner to the input file
+    try {
+      colorValuesInputFile = new File("input/color_values.txt");
+      scanner = new Scanner(colorValuesInputFile);
+    } catch (FileNotFoundException noFile) {
+      System.out.println("Unable to locate file");
+    }
+    // TODO Step One: Read in the RED color value
+
+    // TODO Step Two: Read in the GREEN color value
+
+    // TODO Step Three: Read in the BLUE color value
+
+    // Step Four: Create the JFrame window
+    JFrame window = new JFrame("Gregory M. Kapfhammer " + new Date());
+    // Step Five: Add the drawing canvas and do necessary things to
+    // make the window appear on the screen!
+    window.getContentPane().add(new PaintComplementaryDrawingCanvas());
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // Step Six: ensure that the window is visible, enabling it to display boxes
+
+    // Step Seven: set the size for the window that will contain the colored boxes
+
+  }
+}
---
+++ src/main/java/practicalfour/PaintComplementaryDrawingCanvas.java
@@ -0,0 +1,53 @@
+package practicalfour;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JApplet;
+
+/**
+ * This class paints boxes using a complementary color scheme.
+ *
+ * <p>Bugs: This program has no known bugs, but is missing code.
+ *
+ * @author Janyl Jumadinova
+ */
+public class PaintComplementaryDrawingCanvas extends JApplet {
+
+  // declare the variables for the origin of the canvas
+  private static final int HORIZONTAL_ORIGIN = 0;
+  private static final int VERTICAL_ORIGIN = 0;
+
+  // define the CANVAS_HEIGHT and CANVAS_WIDTH of the graphic
+  public static final int CANVAS_WIDTH = 600;
+  public static final int CANVAS_HEIGHT = 400;
+
+  // TODO: define the maximum RGB value that is possible
+
+  /**
+   * This method paints boxes using a complementary color scheme.
+   *
+   * @author Janyl Jumadinova
+   */
+  public void paint(Graphics page) {
+    // Create the color based on the input values from the user
+    Color userColor =
+        new Color(DisplayComplementaryDrawingCanvas.redValue,
+                  DisplayComplementaryDrawingCanvas.greenValue,
+                  DisplayComplementaryDrawingCanvas.blueValue);
+
+    // TODO: fill the first half (left-to-right) with the user's color
+    // make a call to page.fillRect with the correct parameters
+
+
+    // TODO: calculate the "complementary" color of the provided color
+    // and then create a new Color object called userComplementaryColor.
+    // Refer to notes in the practical assignment sheet about this calculation.
+    // Make sure to add your own calculation to replace the value of null!
+    Color userComplementaryColor = null;
+
+    // TODO: fill the second half (left-to-right) with the complement of the user's color
+    // make a call to page.fillRect with the correct parameters
+    page.setColor(userComplementaryColor);
+
+  }
+}
